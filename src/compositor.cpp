b9135615fa4ae08c46e49b201a38ca9118a9c5b4
Mohammed Sameer <msameer@foolab.org>
2015-10-10 03:05:59 +0300

    unset full screen surface parent when it is cleared

--- src/compositor.cpp
+++ src/compositor.cpp
@@ -38,6 +38,7 @@ void Compositor::setFullScreenSurface(QWaylandQuickSurface *surface) {
     QWaylandSurfaceItem *item = Compositor::item(m_fullScreen);
     item->setTouchEventsEnabled(false);
     defaultInputDevice()->setKeyboardFocus(0);
+    item->setParentItem(0);
   }
 
   m_fullScreen = surface;
